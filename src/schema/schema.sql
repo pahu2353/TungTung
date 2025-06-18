129a20b4f55738ff68c72d2e10e90ef40ebbec07
sbrina-w <sabrinawang93@gmail.com>
2025-06-17 22:53:01 -0400

    add constraints

--- src/schema/schema.sql
+++ src/schema/schema.sql
@@ -4,7 +4,9 @@ CREATE TABLE Accounts (
   profile_picture TEXT,
   phone_number VARCHAR(100),
   email VARCHAR(100),
-  overall_rating FLOAT DEFAULT NULL
+  overall_rating FLOAT DEFAULT NULL,
+  CONSTRAINT check_contact CHECK (phone_number IS NOT NULL OR email IS NOT NULL),
+  CONSTRAINT check_overall_rating CHECK (overall_rating IS NULL OR (overall_rating >= 1.0 AND overall_rating <= 5.0))
 );
 
 CREATE TABLE TaskCategories (
@@ -17,13 +19,13 @@ CREATE TABLE Listings (
   listing_name VARCHAR(100) NOT NULL,
   description TEXT,
   poster_uid INT NOT NULL,
-  capacity INT DEFAULT 1,
-  price DECIMAL(10,2) NOT NULL,
-  duration INT NOT NULL,
+  capacity INT DEFAULT 1 CHECK (capacity > 0),
+  price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
+  duration INT NOT NULL CHECK (duration > 0),
   address VARCHAR(255) NOT NULL,  
   longitude DECIMAL(9,6) NOT NULL,
   latitude DECIMAL(9,6) NOT NULL,
-  posting_time TIMESTAMP NOT NULL,
+  posting_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   deadline TIMESTAMP,
   status ENUM('open', 'taken', 'completed', 'cancelled'),
   FOREIGN KEY (poster_uid) REFERENCES Accounts(uid)
@@ -60,8 +62,10 @@ CREATE TABLE Reviews (
   reviewee_uid INT,
   rating INT CHECK (rating BETWEEN 1 AND 5),
   comment TEXT,
-  timestamp TIMESTAMP,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (listid) REFERENCES Listings(listid),
   FOREIGN KEY (reviewer_uid) REFERENCES Accounts(uid),
-  FOREIGN KEY (reviewee_uid) REFERENCES Accounts(uid)
+  FOREIGN KEY (reviewee_uid) REFERENCES Accounts(uid),
+  CONSTRAINT check_no_self_review CHECK (reviewer_uid != reviewee_uid),
+  UNIQUE unique_review(listid, reviewer_uid, reviewee_uid)
 );
