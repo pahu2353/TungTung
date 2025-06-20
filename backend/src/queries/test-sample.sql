1969396afdd931e131bcdc913ae9029934df5c42
pahu2353 <pahu2353@gmail.com>
2025-06-19 23:48:55 -0400

    Fix test-sample.sql and run test-sample.out

--- backend/src/queries/test-sample.sql
+++ backend/src/queries/test-sample.sql
@@ -7,7 +7,7 @@ INSERT INTO Accounts(name, phone_number, email) VALUES('William Huang', '647-555
 INSERT INTO Listings
   (listing_name, description, poster_uid, capacity, price, duration, address, longitude, latitude, deadline, status)
 VALUES
-  ('Window Cleaning', 'I need the best of the best, will pay extra in tips for a good job!', 1, 2, 45.45, 1, '99 Queen St, Toronto, ON', 123.9, 44.2, '2017-07-23', 'open');
+  ('Window Cleaning', 'I need the best of the best, will pay extra in tips for a good job!', 1, 2, 45.45, 1, '99 Queen St, Toronto, ON', 123.9, 44.2, '2026-07-23', 'open');
 
 -- OUTPUT TABLE
 -- listid  listing_name     description   poster_uid capacity price duration address     longitude latitude deadline   status
@@ -16,7 +16,7 @@ VALUES
 INSERT INTO ListingAssignment(listid, uid)
 SELECT listid, uid
 FROM Accounts, Listings
-WHERE Listings.listid = 1 AND uid = 2 AND uid != poster_uid AND Listing.status = 'open' AND (
+WHERE Listings.listid = 1 AND uid = 2 AND uid != poster_uid AND (
   SELECT COUNT(*)
   FROM ListingAssignment
   WHERE ListingAssignment.listid = Listings.listid
@@ -26,12 +26,16 @@ WHERE Listings.listid = 1 AND uid = 2 AND uid != poster_uid AND Listing.status =
 -- listid uid
 -- 1      2
 
+UPDATE Listings
+SET status = 'completed'
+WHERE listid = 1;
+
 INSERT INTO Reviews(listid, reviewer_uid, reviewee_uid, rating, comment)
-VALUES(1, 1, 2, 3, 'HORRIBLE CLEANING SERVICE I HATE YOU');
+VALUES(1, 2, 1, 1, 'HORRIBLE CLEANING SERVICE I HATE YOU');
 
 -- OUTPUT TABLE
 -- review_id listid reviewer_uid reviewee_uid rating comment                               timestamp
--- 1         1      1            2            3      HORRIBLE CLEANING SERVICE I HATE YOU  06-19-2025    
+-- 1         1      2            1            1      HORRIBLE CLEANING SERVICE I HATE YOU  06-19-2025    
 
 SELECT * FROM TaskCategories;
 
