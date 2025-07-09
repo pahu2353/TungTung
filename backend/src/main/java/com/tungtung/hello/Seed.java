1472cdc6270cd4d82f25bbf79e2df15e302a266c
William Li <52115161+Trollermaner@users.noreply.github.com>
2025-07-08 20:12:07 -0400

    sum changes

--- backend/src/main/java/com/tungtung/hello/Seed.java
+++ backend/src/main/java/com/tungtung/hello/Seed.java
@@ -1,7 +1,9 @@
 package com.tungtung.hello;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -9,48 +11,86 @@ import com.github.javafaker.Faker;
 
 public class Seed {
   private final JdbcTemplate jdbc;
-  private List<Object[]> userList;
+  private Faker faker;
 
   public Seed(JdbcTemplate jdbc) {
     this.jdbc = jdbc;
-    this.userList = new ArrayList<>();
+    this.faker = new Faker();
   }
 
   public void clearDb() {
-    String sql = """
-        DELETE FROM Accounts
-        """;
-  }
+    jdbc.execute("SET FOREIGN_KEY_CHECKS = 0");
+    
+    jdbc.batchUpdate(
+      "TRUNCATE TABLE Users",
+      "TRUNCATE TABLE Reviews",
+      "TRUNCATE TABLE AssignedTo",
+      "TRUNCATE TABLE InterestedIn",
+      "TRUNCATE TABLE BelongsTo",
+      "TRUNCATE TABLE Listings",
+      "TRUNCATE TABLE TaskCategories"
+    );
+    
+    jdbc.execute("SET FOREIGN_KEY_CHECKS = 1");
+}
+
 
   public void createAccountsBatch() {
     String sql = """
-      INSERT INTO Accounts
+      INSERT INTO Users
         (name, profile_picture, phone_number, email)
       VALUES (?, ?, ?, ?)
       """;
-    Faker faker = new Faker();
+    List<Object[]> userList = new ArrayList<>();
 
     for (int i = 0; i < 50; i++) {
-        String name  = faker.name().firstName() + " " + faker.name().lastName();
-        String pfp   = faker.internet().avatar();
-        String phone = faker.phoneNumber().phoneNumber();
-        String email = faker.internet().emailAddress();
+        String name  = this.faker.name().firstName() + " " + this.faker.name().lastName();
+        String pfp   = this.faker.internet().avatar();
+        String phone = this.faker.phoneNumber().phoneNumber();
+        String email = this.faker.internet().emailAddress();
 
-        this.userList.add(new Object[]{ name, pfp, phone, email });
+        userList.add(new Object[]{ name, pfp, phone, email });
     }
 
-    jdbc.batchUpdate(sql, this.userList);
+    jdbc.batchUpdate(sql, userList);
+  }
+
+  public void createListings() {
+    int postingId = 1;
+    Random rnd = new Random();
+    for (int id = 1; id <= 50; id++) {
+      int postings = rnd.nextInt(6);
+      for (int posting = 0; posting < postings; posting++, postingId++) {
+        // make posting for user 'id'
+
+        // pick categories for each posting
+        int categories = rnd.nextInt(4);  // 0–3 distinct picks
+
+        List<Integer> pool = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) pool.add(i);
+
+        Collections.shuffle(pool, rnd);
+
+        for (int i = 0; i < categories; i++) {
+          int pick = pool.get(i);
+        }
+      }
+    }
   }
 
   public void createCategories() {
+    String sql = "INSERT INTO TaskCategories(category_name) VALUES(?)";
+    List<Object[]> genreList = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      String category = this.faker.music().genre();
 
+      genreList.add(new Object[]{category});
+    }
+
+    jdbc.batchUpdate(sql, genreList);
   }
 
   public void createReviews() {
 
-  }
-
-  public void createListings() {
-    
   }
 }
--- backend/src/queries/test-production.sql
+++ backend/src/queries/test-production.sql
@@ -1,18 +1,30 @@
-INSERT INTO Accounts(name, phone_number, email) VALUES('William Huang', '647-555-1192', 'william@uwaterloo.ca');
+INSERT INTO Users(name, phone_number, email) VALUES('William Huang', '647-555-1192', 'william@uwaterloo.ca');
+
+-- OUTPUT TABLE
+-- uid name           profile_picture phone_number email                overall_rating
+-- 1   William Huang  NULL            647-555-1192 william@uwaterloo.ca NULL
 
 INSERT INTO Listings
   (listing_name, description, poster_uid, capacity, price, duration, address, longitude, latitude, deadline, status)
 VALUES
   ('Window Cleaning', 'I need the best of the best, will pay extra in tips for a good job!', 1, 2, 45.45, 1, '99 Queen St, Toronto, ON', 123.9, 44.2, '2026-07-23', 'open');
 
-INSERT INTO ListingAssignment(listid, uid)
+-- OUTPUT TABLE
+-- listid  listing_name     description   poster_uid capacity price duration address     longitude latitude deadline   status
+-- 1       Window Cleaning  I need the... 1          2        45.45 1        99 Queen... 123.9     44.2     2026-07-23 open
+
+INSERT INTO AssignedTo(listid, uid)
 SELECT listid, uid
-FROM Accounts, Listings
+FROM Users, Listings
 WHERE Listings.listid = 1 AND uid = 2 AND uid != poster_uid AND (
   SELECT COUNT(*)
-  FROM ListingAssignment
-  WHERE ListingAssignment.listid = Listings.listid
+  FROM AssignedTo
+  WHERE AssignedTo.listid = Listings.listid
 ) < Listings.capacity;
+
+-- OUTPUT TABLE
+-- listid uid
+-- 1      2
 
 UPDATE Listings
 SET status = 'completed'
@@ -21,5 +33,16 @@ WHERE listid = 1;
 INSERT INTO Reviews(listid, reviewer_uid, reviewee_uid, rating, comment)
 VALUES(1, 2, 1, 1, 'HORRIBLE CLEANING SERVICE I HATE YOU');
 
+-- OUTPUT TABLE
+-- review_id listid reviewer_uid reviewee_uid rating comment                               timestamp
+-- 1         1      2            1            1      HORRIBLE CLEANING SERVICE I HATE YOU  06-19-2025    
+
 SELECT * FROM TaskCategories;
 
+-- OUTPUT TABLE
+-- category_id category_name
+-- 0           Gardening
+-- 1           Tutoring
+-- 2           Plumbing
+-- 3           Moving
+-- 4           Cleaning
