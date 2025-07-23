1b46e86e787ff2e47039efc3b5bcebb4de63ced1
Brian Zhang <brian.w.zhang@gmail.com>
2025-07-23 02:33:52 -0400

    remove node modules from root

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-center" />
         <UserProvider>
+          <Toaster position="top-center" />
           {children}
         </UserProvider>
       </body>
